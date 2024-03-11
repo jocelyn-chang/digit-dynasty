fc85a576f81748e8956633584c39ecd580f25520
Trinity Ung <tung3@uwo.ca>
2024-03-11 19:12:38 -0400

    add uppercase to class names

--- login.py
+++
@@ -1,8 +0,0 @@
-# Initialize an empty dictionary to store users
-users = {}
-
-
-
-# Function to add a new user
-def add_user(username, name, age, location):
-    users[username] = {'name': name, 'age': age, 'location': location}
