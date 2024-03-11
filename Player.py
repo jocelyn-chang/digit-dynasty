dd8b5843f00be8e8ab5d4d8228a7dde88cbe6588
Jocelyn Chang <jchan973@uwo.ca>
2024-03-11 19:14:23 -0400

    Player update

--- Player.py
+++ Player.py
@@ -1,12 +1,18 @@
 class Player: 
-    def __init__ (self, username, best_game, high_score_add, high_score_mul, high_score_div, high_score_sub, games):
+    def __init__ (self, username, best_game, best_score, add_score, mul_score, div_score, sub_score, games):
         self.username = username
         self.best_game = best_game
-        self.high_score_add = high_score_add
-        self.high_score_mul = high_score_mul
-        self.high_score_div = high_score_div
-        self.high_score_sub = high_score_sub
+        self.best_score = best_score
+        self.add_score = add_score
+        self.mul_score = mul_score
+        self.div_score = div_score
+        self.sub_score = sub_score
         self.games = games
     
     def add_game (self, game):
         self.games.append(game)
+
+    def update_add (self, new_add_score):
+        self.high_score_add = new_add_score
+
+    #def update_mul (self, new_)
