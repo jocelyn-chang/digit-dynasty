26d490a553a8d7dcb9ae77616ce3d00aac710848
Jocelyn Chang <jchan973@uwo.ca>
2024-03-11 18:43:29 -0400

    Created Player class

---
+++ Player.py
@@ -0,0 +1,8 @@
+class Player: 
+    def __init__ (self, username, best_game, high_score_add, high_score_mul, high_score_div, high_score_sub):
+        self.username = username
+        self.best_game = best_game
+        self.high_score_add = high_score_add
+        self.high_score_mul = high_score_mul
+        self.high_score_div = high_score_div
+        self.high_score_sub = high_score_sub
