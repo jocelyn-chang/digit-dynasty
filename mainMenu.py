4af4711384fba114bcc8fb354c8516979059ff5b
Jocelyn Chang <jchan973@uwo.ca>
2024-03-15 12:36:33 -0400

    Fixed stuff in main menu

--- mainMenu.py
+++ mainMenu.py
@@ -1,18 +1,20 @@
-import pygame
-
-pygame.init()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
-run = True
-while run:
+# import pygame
 
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
+# pygame.init()
 
-pygame.quit()
+# SCREEN_WIDTH = 800
+# SCREEN_HEIGHT = 600
+
+# screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+# run = True
+# while run:
+
+#   for event in pygame.event.get():
+#     if event.type == pygame.QUIT:
+#       run = False
+
+# pygame.quit()
