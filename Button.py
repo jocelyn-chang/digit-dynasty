ed5eefb0c5717d42a4b8a0c78bb7e272aee817aa
Audrey <ali644@uwo.ca>
2024-03-15 14:33:20 -0400

    s

---
+++ Button.py
@@ -0,0 +1,18 @@
+class Button():
+    def __init__(self, image, pos, base_colour, hovering_colour):
+        self.image = image
+        self.x_pos = pos[0]
+        self.y_pos = pos[1]
+        self.base_colour, self.hovering_colour = base_colour, hovering_colour
+        self.rect = self.image.get_rect(center = (self.x_pos, self.y_pos))
+        self.text_rect = self.text.get_rect(center = (self.x_pos, self.y_pos))
+
+        def update(self, screen):
+            if self.image is not None:
+                screen.blit(elf.image, self.rect)
+            screen.blit(self.text, self.text_rect)
+
+        def checkInput(self, position):
+            if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+                return True
+            return False
--- mainMenu.py
+++ mainMenu.py
@@ -10,9 +10,6 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 background = pygame.image.load("images/Background.png")
 
-# Return text in desired size
-def get_font(size):
-
 # Main Menu screen
 def main_menu():
     pygame.display.set_caption("Menu")
@@ -44,23 +41,5 @@ while run:
 
 pygame.quit()
 
-class Button():
-    def __init__(self, image, pos, base_colour, hovering_colour):
-        self.image = image
-        self.x_pos = pos[0]
-        self.y_pos = pos[1]
-        self.base_colour, self.hovering_colour = base_colour, hovering_colour
-        self.rect = self.image.get_rect(center = (self.x_pos, self.y_pos))
-        self.text_rect = self.text.get_rect(center = (self.x_pos, self.y_pos))
-
-        def update(self, screen):
-            if self.image is not None:
-                screen.blit(elf.image, self.rect)
-            screen.blit(self.text, self.text_rect)
-
-        def checkInput(self, position):
-            if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-                return True
-            return False
 
     
