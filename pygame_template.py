e842e594a678902aa20305d80ec4ee33d9c5cbdb
hanwen <hluo226@uwo.ca>
2024-03-15 12:15:38 -0400

    add colours

--- pygame_template.py
+++ pygame_template.py
@@ -6,6 +6,21 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('DIGIT DYNASTY')
+
+# Main Colours
+gold1 = (230, 224, 174)
+gold2 = (223, 188, 94)
+red1 = (238, 97, 70)
+red2 = (215, 60, 55)
+red3 = (181, 31, 9)
+
+# Secondary Colours
+green1 = (116, 217, 219)
+green2 = (153, 216, 196)
+green3 = (113, 182, 135)
+green4 = (88, 133, 120)
+green5 = (117, 132, 133)
 
 run = True
 while run:
