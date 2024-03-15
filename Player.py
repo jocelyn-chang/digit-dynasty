b60a546fd526e67068800bf179ac694d2fa58c94
Jocelyn Chang <jchan973@uwo.ca>
2024-03-15 12:26:16 -0400

    Finished class for now

--- Player.py
+++ Player.py
@@ -30,28 +30,33 @@ class Player:
     def update_sub (self, new_sub_score):
         self.sub_score = new_sub_score
 
+    # get Player's name
     def get_name (self):
         return self.name
     
+    # get Player's games
     def get_games(self):
         return self.games
     
+    # get Player's best game
     def get_best_game (self):
         return self.best_game
     
+    # get Player's addition score
     def get_add (self):
         return self.add_score
     
+    # get Player's multiply score
     def get_mul (self):
         return self.mul_score
     
+    # get Player's division score
     def get_div (self):
         return self.div_score
     
+    # get Player's subtraction score
     def get_sub (self):
         return self.sub_score
-    
-    
     
 
     
