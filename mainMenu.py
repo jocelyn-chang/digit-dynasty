fd5e33d19c89e7e415445a6e9d3f68465f264eeb
Audrey <ali644@uwo.ca>
2024-03-15 15:29:53 -0400

    main menu

--- mainMenu.py
+++ mainMenu.py
@@ -10,19 +10,6 @@ SCREEN_HEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 BACKGROUND = pygame.image.load("images/background.png")
 
-# Main Menu screen
-def main_menu():
-
-    while True:
-        SCREEN.blit(BACKGROUND, (0, 0))
-
-        MENU_MOUSE_POS = pygame.mouse.get_pos()
-
-        START_BUTTON = Button(image = pygame.image.load("images/new_game.png"), pos = (640, 250))
-        LOAD_BUTTON = Button(image = pygame.image.load("images/load_game.png"), pos = (640, 350))
-        HIGH_SCORE_BUTTON = Button(image = pygame.image.load("image/high_score.png"), pos = (640, 450))
-        INSTRUCTION_BUTTON = Button(image = pygame.image.load("image/instructions.png"), pos = (640, 550))
-        EXIT_BUTTON = Button(image = pygame.image.load("images/exit.png"), pos = (640, 650))
 
         
 
@@ -39,16 +26,40 @@ def instructions():
 # Controls screen
 def controls():
 
+# Main Menu screen
+def main_menu():
 
+    while True:
+        SCREEN.blit(BACKGROUND, (0, 0))
 
-run = True
-while run:
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
+        START_BUTTON = Button(image = pygame.image.load("images/new_game.png"), pos = (640, 250))
+        LOAD_BUTTON = Button(image = pygame.image.load("images/load_game.png"), pos = (640, 350))
+        HIGH_SCORE_BUTTON = Button(image = pygame.image.load("image/high_score.png"), pos = (640, 450))
+        INSTRUCTIONS_BUTTON = Button(image = pygame.image.load("image/instructions.png"), pos = (640, 550))
+        EXIT_BUTTON = Button(image = pygame.image.load("images/exit.png"), pos = (640, 650))
 
-pygame.quit()
+        for event in [START_BUTTON, LOAD_BUTTON, HIGH_SCORE_BUTTON, INSTRUCTIONS_BUTTON, EXIT_BUTTON]:
+            button.changeColour(MENU_MOUSE_POS)
+            button.update(SCREEN)
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if START_BUTTON.checkInput(MENU_MOUSE_POS):
+                    start_game()
+                if LOAD_BUTTON.checkInput(MENU_MOUSE_POS):
+                    load_game()
+                if HIGH_SCORE_BUTTON.checkInput(MENU_MOUSE_POS):
+                    high_score()
+                if INSTRUCTIONS.checkInput(MENU_MOUSE_POS):
+                    instructions()
+                if QUIT_BUTTON.checkInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+        pygame.dsplay.update()
 
-    
+main_menu()
