086b55f37b9ed63e7776bdf7c1dc57f4bf7c04ca
Trinity Ung <tung3@uwo.ca>
2024-03-18 19:16:15 -0400

    import background

--- CookingGame.py
+++ CookingGame.py
@@ -1,1 +1,12 @@
 import pygame
+import time
+import random
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('SNAKE GAME')
+BACKGROUND = pygame.image.load("images/cooking_game.png")
