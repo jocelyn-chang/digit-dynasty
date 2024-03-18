46c915642dcbfdfb27ccf973cdd377d26ebd629f
Ken Sue-A-Quan <ksueaqu@uwo.ca>
2024-03-18 19:56:04 -0400

    new map

--- pygame_template.py
+++ pygame_template.py
@@ -13,6 +13,11 @@ LEFTTEMP = pygame.image.load("images/templeleft.png")
 RIGHTTEMP = pygame.image.load("images/templeright.png")
 TOPTEMP = pygame.image.load("images/templetop.png")
 BOTTOMTEMP = pygame.image.load("images/templebottom.png")
+RESIZED_LEFTTEMP = pygame.transform.scale(LEFTTEMP, (110, 110))
+RESIZED_RIGHTTEMP = pygame.transform.scale(RIGHTTEMP, (110, 110))
+RESIZED_TOPTEMP = pygame.transform.scale(TOPTEMP, (103, 103))
+RESIZED_BOTTOMTEMP = pygame.transform.scale(BOTTOMTEMP, (120, 120))
+
 
 # Main Colours
 gold1 = (230, 224, 174)
@@ -42,7 +47,16 @@ while run:
   mouse_x, mouse_y = pygame.mouse.get_pos()
 
   if (133<mouse_x<180 and 139<mouse_y<202):
-    screen.blit(LEFTTEMP, (105, 109))
+    screen.blit(RESIZED_LEFTTEMP, (110, 92))
+
+  if (372<mouse_x<419 and 32<mouse_y<95):
+    screen.blit(RESIZED_TOPTEMP, (348, -2))
+  
+  if (600<mouse_x<647 and 131<mouse_y<194):
+    screen.blit(RESIZED_RIGHTTEMP, (582, 90))
+  
+  if (363<mouse_x<410 and 383<mouse_y<446):
+    screen.blit(RESIZED_BOTTOMTEMP, (340, 352))
 
 
   # Update the display
