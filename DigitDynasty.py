c2fc57a6fa70e3d83a63750a9c5a84ddee37ca46
hanwen <hluo226@uwo.ca>
2024-03-19 18:02:24 -0400

    edits

--- DigitDynasty.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-pygame.init()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('DIGIT DYNASTY')
