760593c09965de413fb80159fd6d14c29c7b138c
Trinity Ung <tung3@uwo.ca>
2024-03-19 19:37:25 -0400

    add questions to orders

--- CookingGame.py
+++ CookingGame.py
@@ -27,10 +27,7 @@ class CookingGame:
         
         
         
-        self.player = Question("PlayerName", "PlayerPassword", "CookingGame", 100, 10, 10, 10, 5, ["CookingGame"])
-        
-        
-        
+        self.player = Question("PlayerName", "PlayerPassword", "CookingGame", 10, 5, 5, 5, 5, ["CookingGame"])
         
 
     def run(self):
